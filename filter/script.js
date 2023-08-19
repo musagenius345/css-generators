1b29deb654d7c03d773cda3362b6bf815f59bb49
musagenius345 <musabadru@gmail.com>
2023-08-19 09:45:27 +0300

    style & js refactor I

--- filter/script.js
+++ filter/script.js
@@ -1,49 +1,44 @@
-new ClipboardJS('.clipboard')
-let root = document.documentElement;
-const offsetX = document.getElementById('offset-x')
-const offsetY = document.getElementById('offset-y')
-const blurRadius = document.getElementById('blur-radius')
-const colorPicker = document.getElementById('color')
-const hueRotate = document.getElementById('hue-rotate')
-const values = document.querySelector('.copy')
-let offsetXValue = '0rem',
-  offsetYValue = '0rem',
-  blurRadiusValue = '0rem',
-  colorPickerValue = '#000000'
-hueRotateValue = '-90deg'
+new ClipboardJS('.clipboard');
 
+const root = document.documentElement;
+const offsetX = document.getElementById('offset-x');
+const offsetY = document.getElementById('offset-y');
+const blurRadius = document.getElementById('blur-radius');
+const colorPicker = document.getElementById('color');
+const hueRotate = document.getElementById('hue-rotate');
+const values = document.querySelector('.copy');
 
+let offsetXValue = '0rem';
+let offsetYValue = '0rem';
+let blurRadiusValue = '0rem';
+let colorPickerValue = '#000000';
+let hueRotateValue = '-90deg';
 
-offsetX.addEventListener('input', (e) => {
-  root.style.setProperty('--offsetX', `${e.target.value}rem`)
-  offsetXValue = `${e.target.value}rem`
-  updateValues()
-})
+function cssProperty(element, event, customProp, units) {
+  element.addEventListener(event, (e) => {
+    const propValue = `${e.target.value}${units}`;
+    root.style.setProperty(`--${customProp}`, propValue);
+    updateValues();
+  });
+}
 
-offsetY.addEventListener('input', (e) => {
-  root.style.setProperty('--offsetY', `${e.target.value}rem`)
-  offsetYValue = `${e.target.value}rem`
-  updateValues()
-})
-
-blurRadius.addEventListener('input', (e) => {
-  root.style.setProperty('--blurRadius', `${e.target.value}rem`)
-  blurRadiusValue = `${e.target.value}rem`
-  updateValues()
-})
-hueRotate.addEventListener('input', (e) => {
-  root.style.setProperty('--hueRotate', `${e.target.value}`)
-  hueRotateValue = `${e.target.value}deg`
-  updateValues()
-})
+cssProperty(offsetX, 'input', 'offsetX', 'rem');
+cssProperty(offsetY, 'input', 'offsetY', 'rem');
+cssProperty(blurRadius, 'input', 'blurRadius', 'rem');
+cssProperty(hueRotate, 'input', 'hueRotate', 'deg');
 
 colorPicker.addEventListener('change', (e) => {
-  root.style.setProperty('--color', e.target.value)
-  colorPickerValue = e.target.value
-  console.log(colorPickerValue)
-  updateValues()
-})
+  root.style.setProperty('--color', e.target.value);
+  colorPickerValue = e.target.value;
+  updateValues();
+});
 
 function updateValues() {
-  values.textContent = `filter: drop-shadow(${offsetXValue} ${offsetYValue} ${blurRadiusValue} ${colorPickerValue} hue-rotate(${hueRotateValue});`
+  const currentOffsetXValue = getComputedStyle(root).getPropertyValue('--offsetX');
+  const currentOffsetYValue = getComputedStyle(root).getPropertyValue('--offsetY');
+  const currentBlurRadiusValue = getComputedStyle(root).getPropertyValue('--blurRadius');
+  const currentHueRotateValue = getComputedStyle(root).getPropertyValue('--hueRotate');
+
+  values.textContent = `filter: drop-shadow(${currentOffsetXValue} ${currentOffsetYValue} ${currentBlurRadiusValue} ${colorPickerValue} hue-rotate(${currentHueRotateValue}));`;
 }
+
