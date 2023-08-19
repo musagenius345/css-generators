8b09f6e7e8eaa67f9d332ffe1362e3c21f9be7c5
musagenius345 <musabadru@gmail.com>
2023-08-19 10:00:57 +0300

    js refactor II

--- filter/script.js
+++ filter/script.js
@@ -1,5 +1,4 @@
-new ClipboardJS('.clipboard');
-
+new ClipboardJS('.clipboard')
 const root = document.documentElement;
 const offsetX = document.getElementById('offset-x');
 const offsetY = document.getElementById('offset-y');
@@ -8,37 +7,58 @@ const colorPicker = document.getElementById('color');
 const hueRotate = document.getElementById('hue-rotate');
 const values = document.querySelector('.copy');
 
-let offsetXValue = '0rem';
-let offsetYValue = '0rem';
-let blurRadiusValue = '0rem';
-let colorPickerValue = '#000000';
-let hueRotateValue = '-90deg';
 
-function cssProperty(element, event, customProp, units) {
+// let offsetXValue = '0rem';
+// let offsetYValue = '0rem';
+// let blurRadiusValue = '0rem';
+// let hueRotateValue = '-90deg';
+let colorPickerValue = '#000000';
+
+// Define a reusable function for updating a CSS property
+function updateCssProperty(element, customProp, value, units) {
+  root.style.setProperty(`--${customProp}`, `${value}${units}`);
+}
+
+// Define a reusable function for setting up event listeners
+function setupEventListener(element, event, customProp, units) {
   element.addEventListener(event, (e) => {
-    const propValue = `${e.target.value}${units}`;
-    root.style.setProperty(`--${customProp}`, propValue);
+    updateCssProperty(root, customProp, e.target.value, units);
     updateValues();
   });
 }
 
-cssProperty(offsetX, 'input', 'offsetX', 'rem');
-cssProperty(offsetY, 'input', 'offsetY', 'rem');
-cssProperty(blurRadius, 'input', 'blurRadius', 'rem');
-cssProperty(hueRotate, 'input', 'hueRotate', 'deg');
+// Define a function to retrieve computed custom property values
+function getComputedCssProperty(customProp) {
+  return getComputedStyle(root).getPropertyValue(`--${customProp}`);
+}
 
+// Setup event listeners for various input elements
+setupEventListener(offsetX, 'input', 'offsetX', 'rem');
+setupEventListener(offsetY, 'input', 'offsetY', 'rem');
+setupEventListener(blurRadius, 'input', 'blurRadius', 'rem');
+setupEventListener(hueRotate, 'input', 'hueRotate', 'deg');
+
+// Setup event listener for color picker
 colorPicker.addEventListener('change', (e) => {
-  root.style.setProperty('--color', e.target.value);
+  updateCssProperty(root, 'color', e.target.value, '');
   colorPickerValue = e.target.value;
   updateValues();
 });
 
+// Define a function to update the displayed CSS values
 function updateValues() {
-  const currentOffsetXValue = getComputedStyle(root).getPropertyValue('--offsetX');
-  const currentOffsetYValue = getComputedStyle(root).getPropertyValue('--offsetY');
-  const currentBlurRadiusValue = getComputedStyle(root).getPropertyValue('--blurRadius');
-  const currentHueRotateValue = getComputedStyle(root).getPropertyValue('--hueRotate');
+  const currentOffsetXValue = getComputedCssProperty('offsetX');
+  const currentOffsetYValue = getComputedCssProperty('offsetY');
+  const currentBlurRadiusValue = getComputedCssProperty('blurRadius');
+  const currentHueRotateValue = getComputedCssProperty('hueRotate');
 
   values.textContent = `filter: drop-shadow(${currentOffsetXValue} ${currentOffsetYValue} ${currentBlurRadiusValue} ${colorPickerValue} hue-rotate(${currentHueRotateValue}));`;
 }
 
+// Initialize the values and update the display
+updateCssProperty(root, 'offsetX', '0', 'rem');
+updateCssProperty(root, 'offsetY', '0', 'rem');
+updateCssProperty(root, 'blurRadius', '0', 'rem');
+updateCssProperty(root, 'hueRotate', '-90', 'deg');
+updateCssProperty(root, 'color', '#000000', '');
+updateValues();
