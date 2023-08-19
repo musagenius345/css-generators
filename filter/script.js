5202b62d691b8105572f6e0854d3016cf9d70477
musagenius345 <musabadru@gmail.com>
2023-08-19 11:09:02 +0300

    Abstracted some functions to util, esm module, some padding

--- filter/script.js
+++ filter/script.js
@@ -1,3 +1,5 @@
+import {setupEventListener, getComputedCssProperty, updateCssProperty} from '../util.js'
+
 /**
  * Initialize ClipboardJS for clipboard functionality.
  */
@@ -51,42 +53,6 @@ const values = document.querySelector('.copy');
  */
 let colorPickerValue = '#000000';
 
-/**
- * Updates a CSS property value.
- * @function
- * @param {HTMLElement} element - The HTML element.
- * @param {string} customProp - The custom property name.
- * @param {string} value - The new value for the property.
- * @param {string} units - The units for the value.
- */
-function updateCssProperty(element, customProp, value, units) {
-  root.style.setProperty(`--${customProp}`, `${value}${units}`);
-}
-
-/**
- * Sets up an event listener to update CSS property values.
- * @function
- * @param {HTMLElement} element - The HTML element.
- * @param {string} event - The event to listen for.
- * @param {string} customProp - The custom property name.
- * @param {string} units - The units for the value.
- */
-function setupEventListener(element, event, customProp, units) {
-  element.addEventListener(event, (e) => {
-    updateCssProperty(root, customProp, e.target.value, units);
-    updateValues();
-  });
-}
-
-/**
- * Retrieves the computed value of a custom CSS property.
- * @function
- * @param {string} customProp - The custom property name.
- * @returns {string} The computed value of the property.
- */
-function getComputedCssProperty(customProp) {
-  return getComputedStyle(root).getPropertyValue(`--${customProp}`);
-}
 
 // Set up event listeners for various input elements
 setupEventListener(offsetX, 'input', 'offsetX', 'rem');
@@ -98,20 +64,20 @@ setupEventListener(hueRotate, 'input', 'hueRotate', 'deg');
 colorPicker.addEventListener('change', (e) => {
   updateCssProperty(root, 'color', e.target.value, '');
   colorPickerValue = e.target.value;
-  updateValues();
+  updateFilterValues();
 });
 
 /**
  * Updates the displayed CSS values.
  * @function
  */
-function updateValues() {
+export function updateFilterValues() {
   const currentOffsetXValue = getComputedCssProperty('offsetX');
   const currentOffsetYValue = getComputedCssProperty('offsetY');
   const currentBlurRadiusValue = getComputedCssProperty('blurRadius');
   const currentHueRotateValue = getComputedCssProperty('hueRotate');
 
-  values.textContent = `filter: drop-shadow(${currentOffsetXValue} ${currentOffsetYValue} ${currentBlurRadiusValue} ${colorPickerValue} hue-rotate(${currentHueRotateValue}));`;
+  values.textContent = `filter: drop-shadow(${currentOffsetXValue} ${currentOffsetYValue} ${currentBlurRadiusValue} ${colorPickerValue}) hue-rotate(${currentHueRotateValue});`;
 }
 
 // Initialize the values and update the display
@@ -120,4 +86,4 @@ updateCssProperty(root, 'offsetY', '0', 'rem');
 updateCssProperty(root, 'blurRadius', '0', 'rem');
 updateCssProperty(root, 'hueRotate', '-90', 'deg');
 updateCssProperty(root, 'color', '#000000', '');
-updateValues();
+updateFilterValues();
---
+++ util.js
@@ -0,0 +1,44 @@
+ import {updateFilterValues} from './filter/script.js'
+ /**
+ * Updates a CSS property value.
+ * @function
+ * @param {HTMLElement} element - The HTML element.
+ * @param {string} customProp - The custom property name.
+ * @param {string} value - The new value for the property.
+ * @param {string} units - The units for the value.
+ */
+const root = document.documentElement;
+ 
+ 
+export function updateCssProperty(element, customProp, value, units) {
+  root.style.setProperty(`--${customProp}`, `${value}${units}`);
+}
+
+
+
+
+/**
+ * Sets up an event listener to update CSS property values.
+ * @function
+ * @param {HTMLElement} element - The HTML element.
+ * @param {string} event - The event to listen for.
+ * @param {string} customProp - The custom property name.
+ * @param {string} units - The units for the value.
+ */
+export function setupEventListener(element, event, customProp, units) {
+  element.addEventListener(event, (e) => {
+    updateCssProperty(root, customProp, e.target.value, units);
+    updateFilterValues();
+  });
+}
+
+
+/**
+ * Retrieves the computed value of a custom CSS property.
+ * @function
+ * @param {string} customProp - The custom property name.
+ * @returns {string} The computed value of the property.
+ */
+export function getComputedCssProperty(customProp) {
+  return getComputedStyle(root).getPropertyValue(`--${customProp}`);
+}
